56359805167a8543084b8c2e98c8a53c2ee686ba
王选中 <wangxuanzhong@syswin.com>
2018-09-15 12:02:01 +0800

    AES

--- temail-vault/src/main/java/com/syswin/temail/kms/vault/exceptions/VaultCipherException.java
+++ temail-vault/src/main/java/com/syswin/temail/kms/vault/exceptions/VaultCipherException.java
@@ -2,7 +2,23 @@ package com.syswin.temail.kms.vault.exceptions;
 
 public class VaultCipherException extends RuntimeException {
 
+  public VaultCipherException() {
+  }
+
   public VaultCipherException(String message) {
     super(message);
   }
+
+  public VaultCipherException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public VaultCipherException(Throwable cause) {
+    super(cause);
+  }
+
+  public VaultCipherException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
 }
