518bf961f37ec8bbd39122ca6d4a853b2fee3693
殷湘 <yinxiang@syswin.com>
2018-09-20 18:49:04 +0800

    added javadoc for cipher interface

--- temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/KeyAwareAsymmetricCipher.java
+++ temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/KeyAwareAsymmetricCipher.java
@@ -1,10 +1,22 @@
 package com.syswin.temail.kms.vault;
 
+import com.syswin.temail.kms.vault.exceptions.VaultCipherException;
 import java.util.Optional;
 
 public interface KeyAwareAsymmetricCipher extends KeyAwareCipher {
 
+  /**
+   * 账户注册，使用其他非对称接口前须先注册
+   * @param userId 账户ID e.g. temail地址
+   * @return 该账户对应的公钥
+   * @throws VaultCipherException 注册账户时或生成密钥失败抛出
+   */
   byte[] register(String userId);
 
+  /**
+   * 根据账户ID获取对应公钥
+   * @param userId 账户ID e.g. temail地址
+   * @return 该账户对应的公钥，若该账户未注册，则返回 {@link Optional#empty()}
+   */
   Optional<byte[]> publicKey(String userId);
 }
--- temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/KeyAwareCipher.java
+++ temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/KeyAwareCipher.java
@@ -1,16 +1,55 @@
 package com.syswin.temail.kms.vault;
 
+import com.syswin.temail.kms.vault.exceptions.VaultCipherException;
+
 public interface KeyAwareCipher {
 
+  /**
+   * 根据用户ID获取对应密钥进行加密
+   * @param userId 账户ID e.g. temail地址
+   * @param plaintext 待加密的明文
+   * @return 明文加密后对应的密文
+   * @throws VaultCipherException 因密钥不正确或用户未注册时抛出异常
+   */
   byte[] encrypt(String userId, String plaintext);
 
+  /**
+   * 根据用户ID获取对应密钥进行解密
+   * @param userId 账户ID e.g. temail地址
+   * @param encryptedBytes 待解密的密文
+   * @return 解密后的明文
+   * @throws VaultCipherException 因密钥不正确或用户未注册时抛出异常
+   */
   String decrypt(String userId, byte[] encryptedBytes);
 
+  /**
+   * 根据用户ID获取对应密钥进行签名
+   * @param userId 账户ID e.g. temail地址
+   * @param plaintext 用于签名的明文
+   * @return 明文对应的签名
+   * @throws VaultCipherException 因密钥不正确或用户未注册时抛出异常
+   */
   byte[] sign(String userId, String plaintext);
 
+  /**
+   * 根据用户ID获取对应密钥进行验签
+   * @param userId 账户ID e.g. temail地址
+   * @param plaintext 用于签名的明文
+   * @param signed 待验签的签名
+   * @return 签名是否与明文匹配
+   * @throws VaultCipherException 因密钥不正确或用户未注册时抛出异常
+   */
   boolean verify(String userId, String plaintext, byte[] signed);
 
+  /**
+   * 删除管理的用户及其密钥
+   * @param userId 账户ID e.g. temail地址
+   */
   void revoke(String userId);
 
+  /**
+   *
+   * @return 该实现的底层算法
+   */
   CipherAlgorithm algorithm();
 }
