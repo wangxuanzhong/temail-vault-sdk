f58462bc143c92c4ed62a470c0373715e34f3e8b
殷湘 <yinxiang@syswin.com>
2018-09-22 15:26:09 +0800

    sdk code tidy up

--- kms-sdk/src/main/java/com/syswin/temail/kms/sdk/KmsSdkAutoConfiguration.java
+++ kms-sdk/src/main/java/com/syswin/temail/kms/sdk/KmsSdkAutoConfiguration.java
@@ -1,59 +1,27 @@
 package com.syswin.temail.kms.sdk;
 
-import com.syswin.temail.kms.sdk.condition.UnWindowsCondition;
-import com.syswin.temail.kms.sdk.condition.WindowsCondition;
 import com.syswin.temail.kms.vault.KeyAwareVault;
+import com.syswin.temail.kms.vault.Vault;
 import com.syswin.temail.kms.vault.VaultKeeper;
-import com.syswin.temail.kms.vault.cache.ICache;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableConfigurationProperties(KmsProperties.class)
 @EnableCaching
-public class KmsSdkAutoConfiguration {
+class KmsSdkAutoConfiguration {
 
   @Bean
-  @ConditionalOnMissingBean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
+  @ConditionalOnExpression("!'${temail.vault.registry.url}'.isEmpty()")
+  KeyAwareVault vaultKeeper(@Value("${temail.vault.registry.url}") String baseUrl, @Value("${temail.vault.registry.token}") String token) {
+    return VaultKeeper.keyAwareVault(baseUrl, token);
   }
 
   @Bean
-  public KeyAwareVault vaultKeeper(@Value("${temail.vault.registry.url}") String baseUrl) {
-    return VaultKeeper.keyAwareVault(baseUrl, UUID.randomUUID().toString());
-  }
-
-  @Bean
-  public TSBService itsbService(CipherService cipherService, ICache iCache) {
-    return new TSBService(cipherService, iCache);
-  }
-
-  @Bean
-  @Conditional(WindowsCondition.class)
-  public CipherService winCipherService(KmsProperties kmsProperties, RestTemplate restTemplate, KeyAwareVault vaultKeeper) {
-    return new NullCipherService(kmsProperties, restTemplate, vaultKeeper);
-  }
-
-
-  @Bean
-  @Conditional(UnWindowsCondition.class)
-  public CipherService cipherService(KmsProperties kmsProperties, RestTemplate restTemplate, KeyAwareVault vaultKeeper) {
-    return new CipherService(kmsProperties, restTemplate, vaultKeeper);
-  }
-
-  @Bean
-  public ICache kmsCache(ApplicationContext applicationContext) {
-    EhCacheCacheManager ehCacheCacheManager = applicationContext.getBean(EhCacheCacheManager.class);
-    return new KmsCache(ehCacheCacheManager);
+  @ConditionalOnExpression("'${temail.vault.registry.url}'.isEmpty()")
+  Vault vault() {
+    return VaultKeeper.vault();
   }
 }
--- kms-sdk/src/test/java/m/syswin/temail/kms/sdk/test/TestApplication.java
+++ kms-sdk/src/test/java/m/syswin/temail/kms/sdk/test/TestApplication.java
@@ -1,9 +1,9 @@
 package com.syswin.temail.kms.sdk.test;
 
-import com.syswin.temail.kms.sdk.KmsProperties;
-import com.syswin.temail.kms.sdk.TSBService;
-import com.syswin.temail.kms.vault.KeyPair;
-import com.syswin.temail.kms.vault.cache.ICache;
+import static com.syswin.temail.kms.vault.CipherAlgorithm.ECDSA;
+
+import com.syswin.temail.kms.vault.KeyAwareVault;
+import java.util.Optional;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,77 +18,45 @@ public class TestApplication implements CommandLineRunner {
   }
 
   @Autowired
-  TSBService itsbService;
-
-  @Autowired
-  KmsProperties kmsProperties;
-  @Autowired
-  private ICache cache;
+  private KeyAwareVault vault;
 
   @Override
   public void run(String... args) {
-    String key = "key";
-    KeyPair keyPair = new KeyPair("privateKey".getBytes(), "pubKey".getBytes());
-    cache.put(key, keyPair);
-    KeyPair keyPair1 = cache.get(key);
-    Assert.assertEquals(new String(keyPair1.getPrivate()), "privateKey");
-    Assert.assertEquals(new String(keyPair1.getPublic()), "pubKey");
-    System.out.println("cache OK");
-    System.out.println("-------------------------------");
     //temail aes注册
     String temail = "milk" + (System.currentTimeMillis()) + "@temail.com";
-    String encrypted = itsbService.symmetricRegister(temail);
-    System.out.println("symmetricRegister : " + encrypted);
-    //获取密钥
-    String symmetricKey1 = itsbService.getSymmetricKey(temail);
-    System.out.println("symmetricKey : " + symmetricKey1);
-
-    String symmetricKey2 = itsbService.getSymmetricKey(temail);
-    System.out.println("symmetricKey : " + symmetricKey2);
-    Assert.assertEquals(symmetricKey1, symmetricKey2);
+    String symmetricKey = vault.symmetricCipher().getKey(temail);
 
     String text = "milk@t.email中国人";
     //加密
-    String encryptedStr = itsbService.symmetricEncrypt(symmetricKey1, text);
+    String encryptedStr = vault.symmetricCipher().encrypt(symmetricKey, text);
     System.out.println("encryptedStr : " + encryptedStr);
     //解密
-    String decryptedStr = itsbService.symmetricDecrypt(symmetricKey1, encryptedStr);
+    String decryptedStr = vault.symmetricCipher().decrypt(symmetricKey, encryptedStr);
     System.out.println("decryptedStr : " + decryptedStr);
     Assert.assertEquals(text, decryptedStr);
     System.out.println("-------------------------------");
     String eccTemail = "ecctemail" + (System.currentTimeMillis()) + "@temail.com";
     //sdk ecc注册
     String unsign = "this is a text";
-    String signature = itsbService.asymmetricRegister(eccTemail);
+    String signature = vault.asymmetricCipher(ECDSA).register(eccTemail);
     System.out.println("asymmetricRegister : " + signature);
-//    //签名
-//    String sign = itsbService.sign(eccTemail, unsign);
-//    System.out.println("signatureStr : " + sign);
-//    //验证
-//    boolean verifyResult1 = itsbService.asymmetricVerify(eccTemail, unsign, sign);
-//    System.out.println("asymmetricVerifyResult : " + verifyResult1);
-//    boolean verifyResult2 = itsbService.asymmetricVerify(eccTemail, unsign, sign);
-//    System.out.println("asymmetricVerifyResult : " + verifyResult2);
-//    Assert.assertEquals(verifyResult1, verifyResult2);
     //获取公私钥对
-    String asymmetricKey = itsbService.getAsymmetricPubKey(eccTemail);
+    Optional<String> asymmetricKey = vault.asymmetricCipher(ECDSA).publicKey(eccTemail);
     System.out.println("asymmetricKey : " + asymmetricKey);
-    Assert.assertEquals(signature, asymmetricKey);
+    Assert.assertEquals(signature, asymmetricKey.get());
 
-    String txt = itsbService.asymmetricEncrypt(eccTemail, unsign);
-    String a = itsbService.asymmetricDecrypt(eccTemail, txt);
+    String txt = vault.asymmetricCipher(ECDSA).encrypt(eccTemail, unsign);
+    String a = vault.asymmetricCipher(ECDSA).decrypt(eccTemail, txt);
     Assert.assertEquals(a, unsign);
 
     //签名
-    String sign = itsbService.sign(eccTemail, unsign);
+    String sign = vault.asymmetricCipher(ECDSA).sign(eccTemail, unsign);
     System.out.println("signatureStr : " + sign);
     //验证
-    boolean verifyResult1 = itsbService.asymmetricVerify(eccTemail, unsign, sign);
+    boolean verifyResult1 = vault.asymmetricCipher(ECDSA).verify(eccTemail, unsign, sign);
     System.out.println("asymmetricVerifyResult : " + verifyResult1);
-    boolean verifyResult2 = itsbService.asymmetricVerify(eccTemail, unsign, sign);
+    boolean verifyResult2 = vault.asymmetricCipher(ECDSA).verify(eccTemail, unsign, sign);
     System.out.println("asymmetricVerifyResult : " + verifyResult2);
     Assert.assertEquals(verifyResult1, verifyResult2);
-
-    System.exit(0);
   }
 }
