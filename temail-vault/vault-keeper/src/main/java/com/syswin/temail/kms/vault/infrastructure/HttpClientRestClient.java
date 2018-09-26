a49f79f986112c447b1adc6abd89ec49bca59eb5
王选中 <wangxuanzhong@syswin.com>
2018-09-26 15:36:05 +0800

    KMS 修改远程获取方式，换为restTemplate

--- temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/infrastructure/HttpClientRestClient.java
+++ temail-vault/vault-keeper/src/main/java/com/syswin/temail/kms/vault/infrastructure/HttpClientRestClient.java
@@ -1,50 +1,68 @@
 package com.syswin.temail.kms.vault.infrastructure;
 
-import static org.apache.http.HttpVersion.HTTP_1_1;
-import static org.apache.http.entity.ContentType.APPLICATION_JSON;
-
 import com.google.gson.Gson;
 import com.syswin.temail.kms.vault.RestClient;
 import com.syswin.temail.kms.vault.exceptions.VaultCipherException;
-import java.io.ByteArrayOutputStream;
 import java.lang.invoke.MethodHandles;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Request;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class HttpClientRestClient implements RestClient {
+
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private final String baseUrl;
   private final Gson gson = new Gson();
+  private final List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+  private final RestTemplate restTemplate;
 
   public HttpClientRestClient(String baseUrl) {
     this.baseUrl = baseUrl;
+    this.messageConverters.add(new StringHttpMessageConverter());
+    restTemplate = new RestTemplate(messageConverters);
   }
 
   @Override
-  public <REQUEST, RESPONSE> RESPONSE post(String path, REQUEST request, Class<RESPONSE> classType) {
+  public <REQUEST, RESPONSE> RESPONSE post(String path, REQUEST request,
+      Class<RESPONSE> classType) {
     String url = baseUrl + path;
     try {
       LOG.debug("Sending request to remote url {}", url);
-      HttpResponse response = Request.Post(url)
-          .useExpectContinue()
-          .version(HTTP_1_1)
-          .bodyString(gson.toJson(request), APPLICATION_JSON)
-          .execute()
-          .returnResponse();
+//      HttpResponse response = Request.Post(url)
+//          .useExpectContinue()
+//          .version(HTTP_1_1)
+//          .bodyString(gson.toJson(request), APPLICATION_JSON)
+//          .execute()
+//          .returnResponse();
+//
+//      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//      response.getEntity().writeTo(outputStream);
+//      String json = outputStream.toString();
 
-      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-      response.getEntity().writeTo(outputStream);
-      String json = outputStream.toString();
-
-      int statusCode = response.getStatusLine().getStatusCode();
+      HttpHeaders headers = new HttpHeaders();
+      MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+      headers.setContentType(type);
+      headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+      HttpEntity formEntity = new HttpEntity(gson.toJson(request), headers);
+      ResponseEntity<String> response = restTemplate
+          .postForEntity(url, formEntity, String.class);
+      int statusCode = response.getStatusCode().value();
+//      int statusCode = response.getStatusLine().getStatusCode();
+      String json = response.getBody();
       if (statusCode != 200) {
         LOG.error("Unexpected response from remote url {}: {}", url, json);
-        throw new VaultCipherException(errorMessage(url) + ", status code = " + statusCode + ", response = " + json);
+        throw new VaultCipherException(
+            errorMessage(url) + ", status code = " + statusCode + ", response = " + json);
       }
-
       return gson.fromJson(json, classType);
     } catch (Exception e) {
       LOG.error("Unexpected response from remote url {}", url, e);
