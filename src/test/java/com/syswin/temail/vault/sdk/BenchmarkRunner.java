6ad55c80dd851b1f02e5808f772a684bd0ff8b16
殷湘 <yinxiang@syswin.com>
2018-11-07 15:27:12 +0800

    removed deprecated jni files

--- src/test/java/com/syswin/temail/vault/sdk/BenchmarkRunner.java
+++
@@ -1,8 +0,0 @@
-package com.syswin.temail.vault.sdk;
-
-public class BenchmarkRunner {
-
-    public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
-    }
-}
